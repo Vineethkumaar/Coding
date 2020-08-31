069a16ad29b48092aae42c203f2fb09926d21618
Vineeth Kumar <62930860+Vineethkumaar@users.noreply.github.com>
2020-09-01 02:42:02 +0530

    Create 1089. Duplicate Zeros-LeetCode.cpp

---
+++ 1089. Duplicate Zeros-LeetCode.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void duplicateZeros(vector<int>& arr) {
+        
+        queue <int> q;
+        
+    for ( auto i=0 ; i< arr.size(); i++)
+        {
+
+            q.push(arr[i]);
+            
+            if(arr[i]==0)
+                q.push(0);
+            
+            arr[i]=q.front();
+            q.pop();
+        }
+        
+        
+        
+        
+    }
+};
