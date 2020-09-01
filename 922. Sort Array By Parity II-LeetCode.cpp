d1d70209c134b87446f0e28d66c1c1237c41f60c
Vineeth Kumar <62930860+Vineethkumaar@users.noreply.github.com>
2020-09-01 13:56:20 +0530

    Update 922. Sort Array By Parity II-LeetCode.cpp

--- 922. Sort Array By Parity II-LeetCode.cpp
+++ 922. Sort Array By Parity II-LeetCode.cpp
@@ -42,3 +42,50 @@ public:
         
     }
 };
+
+
+//USING 0 SPACE
+class Solution {
+public:
+    vector<int> sortArrayByParityII(vector<int>& A) {
+        
+        
+        vector< int > v(A.size(),0);
+        int j;
+        
+        for(int i=0 ; i<A.size(); i++)
+      
+        {
+            j=i;
+            
+            if(i%2==0 && A[i]%2!=0)
+            {
+                while(j<A.size())
+                {
+                    if(A[j]%2==0)
+                        break;
+                    j++;
+                }
+                swap(A[i],A[j]);
+            }
+            
+            
+             if(i%2!=0 && A[i]%2==0)
+            {
+                while(j<A.size())
+                {
+                    if(A[j]%2!=0)
+                        break;
+                    j++;
+                }
+                swap(A[i],A[j]);
+            }
+            
+         
+            
+        }
+        
+        return A;
+        
+    }
+};
