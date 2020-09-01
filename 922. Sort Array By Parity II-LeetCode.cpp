d25a8b8a84c1606f0d3fd97495329bccf753c786
Vineeth Kumar <62930860+Vineethkumaar@users.noreply.github.com>
2020-09-01 14:43:28 +0530

    Update 922. Sort Array By Parity II-LeetCode.cpp

--- 922. Sort Array By Parity II-LeetCode.cpp
+++ 922. Sort Array By Parity II-LeetCode.cpp
@@ -89,3 +89,48 @@ public:
         
     }
 };
+//NO extra space and sorting anyone so the other sort by itself.
+class Solution {
+public:
+    vector<int> sortArrayByParityII(vector<int>& A) {
+        
+        
+        vector< int > v(A.size(),0);
+        int j=1;
+        
+        for(int i=0 ; i<A.size(); i=i+2)
+      
+        {
+            
+            if(A[i]%2==1)
+            {
+                while(A[j]%2==1 )
+                {
+
+                    j=j+2;
+                }
+                cout<<A[j];
+                swap(A[i],A[j]);
+            
+            }
+            
+            
+           /*  if(i%2!=0 && A[i]%2==0)
+            {
+                while(j<A.size())
+                {
+                    if(A[j]%2!=0)
+                        break;
+                    j++;
+                }
+                swap(A[i],A[j]);
+            }
+            */
+         
+            
+        }
+        
+        return A;
+        
+    }
+};
