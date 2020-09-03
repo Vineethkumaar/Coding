43a72a0ada41d4bfb66d4236c2c54c2fdc68d4ec
Vineeth Kumar <62930860+Vineethkumaar@users.noreply.github.com>
2020-09-04 01:14:19 +0530

    Update 501. Find Mode in Binary Search Tree-LeetCode.cpp

--- 501. Find Mode in Binary Search Tree-LeetCode.cpp
+++ 501. Find Mode in Binary Search Tree-LeetCode.cpp
@@ -67,3 +67,108 @@ public:
         
     }
 };
+// method 2
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    map<int , int > mp;
+    int mx=0;
+public:
+    vector<int> findMode(TreeNode* root) {
+         vector < int > l;
+      
+        if(root==NULL)
+            return l;
+       
+           pre(root);
+        
+        
+       for(auto x: mp) mx = max(mx, x.second);
+        
+        for(auto x: mp) {
+            if(x.second == mx) l.push_back(x.first);
+        }
+        
+               
+        return l;
+        
+    }
+ 
+    
+    void pre(TreeNode * root)
+    {
+        
+        if(root==NULL) return;
+        
+        mp[root->val]++;
+        
+        
+        pre(root->left);
+        pre(root->right);
+        return;
+        
+        
+    }
+};
+//method 3
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    map<int , int > mp;
+    int mx=0;
+    
+public:
+    vector<int> findMode(TreeNode* root) {
+         vector < int > l;
+      
+        if(root==NULL)
+            return l;
+        
+           pre(root);
+        
+        for(auto x: mp) 
+        {
+            if(x.second == mx) l.push_back(x.first);
+        }
+               
+        return l;
+        
+    }
+ 
+    
+    void pre(TreeNode * root)
+    {
+        
+        if(root==NULL) return;
+        
+        mp[root->val]++;
+        
+        if(mp[root->val] > mx)
+            mx=mp[root->val];
+        
+        
+        pre(root->left);
+        pre(root->right);
+        return;
+        
+        
+    }
+};
