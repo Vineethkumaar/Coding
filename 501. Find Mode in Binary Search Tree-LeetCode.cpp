fc4eedcbbaabd5eb33aed47ab2c9344c0b077122
Vineeth Kumar <62930860+Vineethkumaar@users.noreply.github.com>
2020-09-04 01:00:07 +0530

    Create 501. Find Mode in Binary Search Tree-LeetCode.cpp

---
+++ 501. Find Mode in Binary Search Tree-LeetCode.cpp
@@ -0,0 +1,69 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    map<int , int > mp;
+public:
+      static bool cmp(pair<int, int>& a, pair<int, int>& b) 
+            { 
+                return a.second > b.second; 
+            } 
+    vector<int> findMode(TreeNode* root) {
+       
+        vector < pair<int,int> > v;
+         vector < int > l;
+      
+        if(root==NULL)
+            return l;
+       
+           pre(root);
+        
+        
+        
+         for (auto& it : mp) 
+         { 
+              v.push_back(it); 
+          } 
+        
+         sort(v.begin(), v.end(), cmp); 
+        
+        int x= v[0].second;
+        
+        for(int i=0 ; i<v.size() ; i++)
+        {
+            if(x==v[i].second)
+            l.push_back(v[i].first);
+               else
+                   break;
+        }
+        
+               
+        return l;
+        
+    }
+ 
+    
+    void pre(TreeNode * root)
+    {
+        
+        if(root==NULL) return;
+        
+        
+        
+        mp[root->val]++;
+        
+        pre(root->left);
+        pre(root->right);
+        return;
+        
+        
+    }
+};
