bbb948e960a5973374288249eded28ef21f25b31
Vineeth Kumar <62930860+Vineethkumaar@users.noreply.github.com>
2020-09-07 01:15:26 +0530

    Update 653. Two Sum IV - Input is a BST-LeetCode.cpp

--- 653. Two Sum IV - Input is a BST-LeetCode.cpp
+++ 653. Two Sum IV - Input is a BST-LeetCode.cpp
@@ -10,34 +10,45 @@
  * };
  */
 class Solution {
-    set <int> mp;
+    unordered_set <int> mp;
+bool flg=false;
 public:
     bool findTarget(TreeNode* root, int k) {
         
         if(root==NULL)
             return false;
         
-        pre(root);
+        pre(root,k);
         
-        for(auto i=mp.begin(); i!=mp.end(); i++)
+   /*     for(auto i=mp.begin(); i!=mp.end(); i++)
         {
            auto z=mp.find(k-*i);
             cout<<*i<<*z<<" ";
             if(z!=mp.end() && z!=i)
                 return true;
         }
+        */
         
-        return false;
+        return flg;
         
     }
-    void pre( TreeNode *root)
+    void pre( TreeNode *root,int k)
     {
            if(root==NULL)
             return;
         
+        //    cout<<*i<<*z<<" ";
+           int i=  root->val;
+           auto z=mp.find(k-i);
+            if(z!=mp.end() && i!= *z || flg==true)
+            { 
+                flg=true;
+                return ;
+            }
+        
         mp.insert(root->val);
-        pre(root->left);
-        pre(root->right);
+        pre(root->left,k);
+        pre(root->right,k);
         return;
         
     }
