2d2ac1dc5784cd4cedb957f3926c4d24d3f442cb
Vineeth Kumar <62930860+Vineethkumaar@users.noreply.github.com>
2020-09-09 00:39:50 +0530

    Update 701. Insert into a Binary Search Tree-LeetCode.cpp

--- 701. Insert into a Binary Search Tree-LeetCode.cpp
+++ 701. Insert into a Binary Search Tree-LeetCode.cpp
@@ -42,3 +42,51 @@ public:
     }
     
 };
+
+//iterative
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        
+        TreeNode *node = new TreeNode (val);
+        if(root==NULL)
+            return node;
+        TreeNode *temp=root;
+        
+     while(temp!=NULL)
+     {
+         if(temp->val >= val)
+         {
+             if(temp->left!=NULL)
+                 temp=temp->left;
+             else
+             {
+                 temp->left=node;
+                 break;
+             }
+         }
+         else
+             if(temp->right!=NULL)
+                 temp=temp->right;
+             else
+             {
+                 temp->right=node;
+                 break;
+             }
+     }
+         
+         return root;
+    }     
+   
+};
